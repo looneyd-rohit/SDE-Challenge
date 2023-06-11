8ab8fbfb384eca0acdb7391ebeb542d38d85a974
ROHIT SADHU <rohitsadhupersonal@gmail.com>
2023-06-11 23:07:17 +0530

    Update Day-4-find-all-duplicates-in-an-array.cpp

--- Day-4-find-all-duplicates-in-an-array.cpp
+++ Day-4-find-all-duplicates-in-an-array.cpp
@@ -1,3 +1,4 @@
+// Approach 1: using visited approach
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
@@ -11,6 +12,30 @@ public:
             }else{
                 // marking visited
                 nums[abs(nums[i]) - 1] *= -1;
+            }
+        }
+        return ans;
+    }
+};
+
+// Approach 2: using swap sort
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        // using swap sort
+        // this method works since any duplicate element is present twice, which means after
+        // swap sort process one will be at correct index and one will be at incorrect index.
+        int n = nums.size();
+        vector<int> ans;
+        for(int i=0; i<n; i++){
+            if(nums[i] != nums[nums[i]-1]){
+                swap(nums[i], nums[nums[i]-1]);
+                i--;
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i]!=i+1){
+                ans.push_back(nums[i]);
             }
         }
         return ans;
