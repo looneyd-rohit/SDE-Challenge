0f5fdb21c009100b27767e55fbc7f40cc5eb1a48
ROHIT SADHU <rohitsadhupersonal@gmail.com>
2023-07-23 20:37:53 +0530

    Time: 123 ms (29.27%) | Memory: 39.4 MB (15.62%) - LeetSync

---
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> solve(int num){
+        // base case
+        if(num == 1){
+            TreeNode* t = new TreeNode(0);
+            vector<TreeNode*> v;
+            v.push_back(t);
+            return v;
+        }
+        
+        // leave atleast one for left subtree, take current node as root, and rest for
+        // right sub tree
+        // start current from 2nd node
+        vector<TreeNode*> ans;
+        for(int i=2; i<num; i+=2){
+            auto l = solve(i - 1), r = solve(num - i);
+            for(auto& left: l){
+                for(auto& right: r){
+                    TreeNode* root = new TreeNode(0);
+                    root->left = left;
+                    root->right = right;
+                    ans.push_back(root);
+                }
+            }
+        }
+        return ans;
+    }
+    vector<TreeNode*> allPossibleFBT(int n) {
+        // full binary tree --> each node has exactly two children or zero children
+        return solve(n);
+    }
+};
